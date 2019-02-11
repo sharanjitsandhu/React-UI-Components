cfc6bb67c9962db4b3f486e01259032a02d2fdba
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-02-11 15:30:44 -0800

    Completed the MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+const CardBanner = () => {
+    return ( 
+        <div className='banner'>
+            <div className='inner-banner'>
+                <img 
+                    alt='banner'
+                    className='image-banner'
+                    src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'
+                />
+            </div>
+                <CardContent />
+        </div>
+     );
+};
+ 
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div className='image'>
+            <img 
+                alt='lambda logo'
+                className='thumb-img'
+                src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png'
+                />
+        </div>
+    );
+};
+
+export default ImageThumbnail;

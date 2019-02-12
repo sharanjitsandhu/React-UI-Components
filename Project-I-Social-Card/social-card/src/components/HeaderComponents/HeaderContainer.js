9f68f879d7a14948524816cd2a8af18303f04680
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-02-11 16:14:36 -0800

    Updated App.css file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
-//import HeaderContent from './HeaderContent';
 import HeaderTitle from './HeaderTitle';
 
 const HeaderContainer = () => {

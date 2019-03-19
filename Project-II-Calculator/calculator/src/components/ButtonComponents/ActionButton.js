431a43925693b22c5bcc96898bd259c9b3c09848
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-03-19 14:13:57 -0700

    Created button components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from "react";
 import "./Button.css";
+
+function ActionButton() {
+  return (
+    <div className="action-buttons">
+      <div>action</div>
+    </div>
+  );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,30 @@
 import React from "react";
 import "./Button.css";
+
+function NumberButton() {
+  return (
+    <div>
+      <div className="number-button">
+        <div className="clear">clear</div>
+        <div className="number">
+          <div className="numbers">7</div>
+          <div className="numbers">8</div>
+          <div className="numbers">9</div>
+        </div>
+        <div className="number">
+          <div className="numbers">4</div>
+          <div className="numbers">5</div>
+          <div className="numbers">6</div>
+        </div>
+        <div className="number">
+          <div className="numbers">1</div>
+          <div className="numbers">2</div>
+          <div className="numbers">3</div>
+        </div>
+        <div className="zero">0</div>
+      </div>
+    </div>
+  );
+}
+
+export default NumberButton;
